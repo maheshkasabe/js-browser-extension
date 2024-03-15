3049ce1ceada9c014827c4aca7dcd002d6e972c7
Mahesh <maheshkasbe010@gmail.com>
2024-03-15 22:37:57 +0530

    js browser extension

---
+++ popup.js
@@ -0,0 +1,13 @@
+// popup.js
+'use strict';
+
+let helloButton = document.getElementById("hello-btn");
+
+helloButton.onclick = function (element) {
+  const defaultName = "Real JavaScript";
+  let name = prompt("Enter your name:", defaultName);
+  if (!name) {
+    name = defaultName;
+  }
+  document.getElementById("hello").innerHTML = `Hello, ${name}!`;
+};
